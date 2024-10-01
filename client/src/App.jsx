a8c7cd05bbbc765025b8478f93e2d1342ee5a9ee
cmabdulkareem <cmabdulkareem@gmail.com>
2024-10-01 17:18:35 +0530

    ajxhsj

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,35 +22,27 @@ import AttendenceDetails from "./Components/Admin/AttendenceDetails";
 
 function App() {
 //logic goes here
- 
+
 
   return (
-     <>
-  
-     <Routes>
-      <Route path="/" element={<LabAttendancePage/>}/>
-      <Route path="/submit-name" element={<StartSession/>}/>
-      <Route path="/token-page/:studentID" element={<TokenPage />} />
-      <Route path="/exit-session" element={<ExitSession/>}/>
-
-      <Route path="/admin" element={<AdminDashboard/>}/>
-          <Route path="/student" element={<StudentManagement />}/>
-      
-   
-         <Route path= "/student-list" element={<StudentList/>}/>
-         <Route path="/student-form" element={<AddStudent/>}/>
-         <Route path="/delete-student" element={<DeleteStudent/>}/>
-         <Route path="/edit-student" element={<EditStudent/>}/>
-
-      <Route path="/admin-login" element={<LoginAdmin/>}/>
-      <Route path="/create-admin" element={<CreateAdmin/>}/>
-      <Route path="/attendence" element={<AttendenceDetails/>}/>
-
-
-     </Routes>
-  
-     </>
-     );
+    <>
+      <Routes>
+        <Route path="/" element={<LabAttendancePage />} />
+        <Route path="/submit-name" element={<StartSession />} />
+        <Route path="/token-page/:studentID" element={<TokenPage />} />
+        <Route path="/exit-session" element={<ExitSession />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/student" element={<StudentManagement />} />
+        <Route path="/student-list" element={<StudentList />} />
+        <Route path="/student-form" element={<AddStudent />} />
+        <Route path="/delete-student" element={<DeleteStudent />} />
+        <Route path="/edit-student" element={<EditStudent />} />
+        <Route path="/admin-login" element={<LoginAdmin />} />
+        <Route path="/create-admin" element={<CreateAdmin />} />
+        <Route path="/attendence" element={<AttendenceDetails />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
